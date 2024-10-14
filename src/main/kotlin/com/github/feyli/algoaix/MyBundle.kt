af8b8e7da8f3cea6b66de82ea47f85b71fced939
GitHub Action <action@github.com>
2024-10-14 12:41:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/feyli/algoaix/MyBundle.kt
+++ src/main/kotlin/com/github/feyli/algoaix/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.feyli.algoaix
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/feyli/algoaix/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/feyli/algoaix/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.feyli.algoaix.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.feyli.algoaix.MyBundle
+import com.github.feyli.algoaix.services.MyProjectService
 import javax.swing.JButton
 
 
